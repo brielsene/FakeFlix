1e0e657968bd425ce7f437f0c67871a6605c9bbf
unknown <gabrielsenecosta@hotmail.com>
2022-06-09 23:56:59 -0300

    atualizacao2

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Tela from './src/telas/Tela';
-
+import { useFonts, Inter_400Regular,Inter_700Bold } from '@expo-google-fonts/inter';
 
 import { View, Text } from 'react-native';
 
+
+
 export default function App() {
+  let [fontsCarregadas] = useFonts({
+    "InterBold": Inter_700Bold,
+    "InterRegular":Inter_400Regular,
+
+  });
+
+  if (!fontsCarregadas) {
+    return <View/>;
+  }
   return(
     <Tela/>
   )
--- src/telas/Tela.js
+++ src/telas/Tela.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import { View, Image } from 'react-native';
+import { View, Image, TextInput} from 'react-native';
 import ImgTopo from './assets/netflix.png';
+
+
 
 export default function Tela(){
     return(<>
@@ -9,10 +11,32 @@ export default function Tela(){
             <Text style={estilos.titulo}>Fake Flix</Text>
             <Image source={ImgTopo} style={estilos.imgtopo}/>
         </View> 
-        <View style={estilos.container}>
-            <Text style={estilos.login}>Login:</Text>
-            <Text style={estilos.senha}>Senha:</Text>
+        
+            
+        
+        <View >
+            <TextInput style={estilos.textInput}></TextInput>
         </View>
+        <View style={estilos.container}>
+            
+            <Text style={estilos.login}>Login:</Text>
+            
+            <Text style={estilos.senha}>Senha:</Text>
+           
+            <Text style={estilos.texto3}>Ainda não tem conta?</Text>
+            <Text style={estilos.texto4}>Cadastre-se</Text>
+            
+            
+            
+            
+        </View>
+        
+        
+       
+        
+            
+       
+        
         </>
     )
         
@@ -25,7 +49,6 @@ const estilos = StyleSheet.create({
         backgroundColor:"#1C1B17",
         paddingTop:46,
         alignItems:"center",
-
     },
 
     titulo:{
@@ -34,26 +57,56 @@ const estilos = StyleSheet.create({
         alignItems: "center",
         justifyContent:"center",
         paddingBottom:6,
-
     },
 
     imgtopo:{
         width:"100%",
     },
 
+    retangulo:{
+        //width:106,
+        //height:24,       
+        //paddingBottom:200
+    },
     login:{
-        paddingRight:248,
+        paddingRight:248,       
         fontSize:20,
         color:"#FFFFFF",
-        paddingBottom:16
+        paddingBottom:16,
     },
+   
 
     senha:{
         paddingRight:248,
         fontSize:20,
         color:"#FFFFFF",
-        
+        paddingBottom:35    
+    },
+
+    texto3:{
+        fontSize:18,
+        color:"#FFFFFF",
+        paddingTop:144,
+        fontFamily: "InterRegular",  
+    },
+    texto4:{
+        fontSize:18,
+        color:"#E31E24",
+        paddingTop:12,
+        fontFamily: "InterBold",   
+    },
+
+    textInput:{
+        width:"100%",
+        height:10,
+        borderWidth:1,
+        borderColor:"#000",
+        paddingBottom:6,
+        paddingTop:50,
+      
     }
+    
+   
 
 
 
